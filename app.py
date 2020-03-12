eaec441a5a95246d8fb86d02283bdf5e2be0c354
Brian Kim <yrkim98@uw.edu>
2020-03-12 14:01:31 -0700

    added code to convert python images to matlab

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify
 import matlab.engine
+from PIL import Image
+# python
+
 # You might want to install matlab engine for python
 # 1) install matlab
 # 2) cd to "matlabroot\extern\engines\python
@@ -22,7 +25,28 @@ def helloworld():
 # test_matlab endpoint for testing communication with matlab
 @app.route("/test_matlab")
 def test_matlab():
-    engine = matlab.engine.start_matlab()
-    engine.cd(matlab_folder, nargout=0)
-    response = engine.testmatlab()
+    response = matlab_engine.testmatlab()
     return jsonify(response)
+
+
+@app.route('/get_score/<website>/<imageURL>')
+def get_score():
+    # put code for capture site here
+    website_screenshot = #get site sc here and load it into matlab
+
+
+
+
+# Converts Pillow.Images into matlab matricies.
+# Note: possibly convert code to use URL instead of path for easier use?
+def convert_to_matlab_image(image_path):
+    # Open image using PIL
+    image = Image.open(image_path)
+    # Convert to matlab data types
+    image_matlab = matlab.uint8(list(image.getdata()))
+    image_matlab.reshape((image.size[0], image.size[1], 3))
+    return image_matlab
+
+
+
+
